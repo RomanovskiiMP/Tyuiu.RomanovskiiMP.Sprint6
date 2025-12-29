a7663c1cd0ce5d51acd63cf84c8b4100128aab51
RomanovskiiMP <gavialtheinvicible@gmail.com>
2025-12-29 04:51:48 +0300

    Поправка

---
+++ Tyuiu.RomanovskiiMP.Sprint6.Task0.V15/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.RomanovskiiMP.Sprint6.Task0.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
